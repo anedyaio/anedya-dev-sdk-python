df837d0542481410cb775b957dd92546fcdf1714
DripYeager <saranshjaiswal12@gmail.com>
2023-11-15 12:49:51 +0530

    Defined basic structure in config.py

--- development/anedya-dev-sdk-python/src/config.py
+++ development/anedya-dev-sdk-python/src/config.py
@@ -1,25 +1,54 @@
 # src/config.py
 
-class AnedyaConfig:
-    def __init__(self, api_token, device_id=None, mode='HTTP'):
-        self.api_token = api_token
+from datetime import datetime
+import paho.mqtt.client as mqtt
+import ssl
+import time
+
+class AnedyaSDK:
+    def __init__(self, device_id, api_token, connection_mode, timeout=60, max_buffer_size=10, tls_certificate=None):
         self.device_id = device_id
-        self.mode = mode
+        self.api_token = api_token
+        self.connection_mode = connection_mode
+        self.timeout = timeout
+        self.max_buffer_size = max_buffer_size
+        self.tls_certificate = tls_certificate
+        self.time_format = "%Y-%m-%d %H:%M:%S"
+        self.buffer = []
 
-    @classmethod
-    def from_device_id(cls, api_token, device_id, mode='HTTP'):
-        # Initialize the configuration with device ID
-        return cls(api_token, device_id, mode)
+    def send_data(self, data):
+        timestamp = time.strftime(self.time_format)
+        formatted_data = f"{timestamp} - {data}"
+        self.buffer.append(formatted_data)
 
-    @classmethod
-    def from_tls_certificate(cls, api_token, tls_certificate_path, mode='MQTT'):
-        # Initialize the configuration with TLS certificate
-        # Additional logic to load TLS certificate goes here
-        return cls(api_token, device_id=None, mode)
+    def push_data(self):
+        if len(self.buffer) >= self.max_buffer_size or self.max_buffer_size == 0:
+            self._push_data_to_cloud()
+            self.buffer.clear()
 
-    def to_dict(self):
-        return {
-            'api_token': self.api_token,
-            'device_id': self.device_id,
-            'mode': self.mode
-        }
+    def set_timeout(self, new_timeout):
+        self.timeout = new_timeout
+
+    def set_max_buffer_size(self, new_max_buffer_size):
+        self.max_buffer_size = new_max_buffer_size
+
+    def set_connection_mode(self, new_connection_mode):
+        self.connection_mode = new_connection_mode
+
+    def set_tls_certificate(self, new_tls_certificate):
+        self.tls_certificate = new_tls_certificate
+
+    def _push_data_to_cloud(self):
+        client = self._connect_mqtt()
+        # Implement logic to push data to the cloud based on the connection mode
+        print(f"Pushing data to the cloud: {self.buffer}")
+        client.disconnect()
+
+    def _connect_mqtt(self):
+        client = mqtt.Client()
+        if self.tls_certificate:
+            client.tls_set(certfile=self.tls_certificate, tls_version=ssl.PROTOCOL_TLS)
+        # Connect to the MQTT broker
+        client.connect("mqtt.eclipse.org", 1883, 60)
+        return client
+
---
+++ development/anedya-dev-sdk-python/tests/test_config.py
@@ -0,0 +1,23 @@
+# test_config.py
+from config import AnedyaSDK
+
+def test_sdk_functionality():
+    sdk = AnedyaSDK(device_id="device_id", api_token="api_token", connection_mode="MQTT", timeout=60, max_buffer_size=10, tls_certificate="tls_certificate.pem")
+
+    sdk.send_data("Sensor Data 1")
+    sdk.send_data("Sensor Data 2")
+
+    sdk.push_data()
+
+    sdk.set_timeout(120)
+    sdk.set_max_buffer_size(20)
+
+    sdk.send_data("Sensor Data 3")
+    sdk.push_data()
+
+    sdk.set_connection_mode("HTTP")
+    # Implement logic for retrieving data if needed
+    print("Retrieving data from the cloud")
+
+if __name__ == "__main__":
+    test_sdk_functionality()
