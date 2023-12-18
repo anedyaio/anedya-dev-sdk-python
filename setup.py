64f869a34f48bb29d598aebb5e3eb78a463c1084
anedyasystems <markand.pathak@anedya.io>
2023-12-18 18:11:07 +0530

    0.0.4 release

--- setup.py
+++ setup.py
@@ -9,24 +9,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
 # You should replace 'README.md' with the actual name of your README file.
 
 setup(
-    name='anedya-dev-sdk',
-    version='0.0.4',
+    name="anedya-dev-sdk",
+    version="0.0.4",
     description='Anedya python based SDK for IoT devices. This SDK is currently under development. Future versions may include breaking changes.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Anedya Systems',
-    url='https://github.com/anedyaio/anedya-dev-sdk-pyhton',
+    author="Anedya Systems",
+    url="https://github.com/anedyaio/anedya-dev-sdk-pyhton",
     author_email='support@anedya.io',
     license="Apache License 2.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    python_requires=">=3",
     install_requires=['requests'],  # Add any required dependencies here
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
